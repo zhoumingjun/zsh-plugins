5015fcd5f5d6da94eb1b7edb7c4d4c678f6b2ea4
zhoumingjun <zhoumingjun@sensetime.com>
2022-05-16 00:37:16 +0800

    feat(init): add login and proxy

---
+++ login/login.plugin.zsh
@@ -0,0 +1,27 @@
+if [[ -e "$HOME/.secret/$1" ]] ; then
+
+function lg(){
+source "$HOME/.secret/$1"
+
+code=`oathtool --totp --base32 -d6 $OATH_SECRET`
+
+expect <(cat <<EOD
+spawn ssh $OPTIONS $JUMPER_SERVER
+
+expect  "*password:"
+send "$AD_SECRET\r"
+
+expect "*auth]:"
+send "$code\r"
+
+
+expect "Opt>"
+send "$2\r"
+interact
+
+EOD
+)
+
+}
+
+fi
---
+++ proxy/proxy.plugin.zsh
@@ -0,0 +1,7 @@
+alias tps-local="export http_proxy=socks5://localhost:1080;export https_proxy=socks5://localhost:1080"
+alias tph-local="export http_proxy=http://localhost:1081;export https_proxy=http://localhost:1081"
+
+alias tph-st1="export http_proxy=http://172.16.1.135:3128;export https_proxy=http://172.16.1.135:3128"
+alias tph-st2="export http_proxy=http://10.54.0.22:3128;export https_proxy=http://10.54.0.22:3128"
+
+alias tpu="unset http_proxy; unset https_proxy"
